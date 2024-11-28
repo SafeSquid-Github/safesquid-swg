6ed20fc9d266f26837a5532f39b16ecd7e559034
Manish Kochar <manish@safesquid.net>
2024-11-28 21:05:41 +0530

    corrections

--- usr/local/bin/performance.sh
+++ usr/local/bin/performance.sh
@@ -1,80 +1,98 @@
 #!/bin/bash
 
-L[0]="TimeStamp"
-L[1]="ElapsedTime"
-L[2]="ConnHandled"
-L[3]="ConnClosed"
-L[4]="Xactions"
-L[5]="C.Pool"
-L[6]="SpareThreads"
-L[7]="ThreadsUsed"
-L[8]="ThreadsWaiting"
-L[9]="ThreadsStarting"
-L[10]="ThreadsPrefetching"
-L[11]="ThreadErrors"
-L[12]="SrvConn"
-L[13]="SrvConnFail"
-L[14]="ConnPoolReused"
-L[15]="SrvPool"
-L[16]="KBytesIn"
-L[17]="KBytesOut"
-L[18]="Caching Objects Created in Memory"
-L[19]="Caching Objects Removed from Memory"
-L[20]="DNS Queries Reused"
-L[21]="New DNS Queries"
-L[22]="DNS Query failures"
-L[23]="Total System Memory (KBytes)"
-L[24]="Free System Memory (KBytes)"
-L[25]="VMemory (KBytes)"
-L[26]="RSSMemory (KBytes)"
-L[27]="ShMemory (KBytes)"
-L[28]="CodeMemory (KBytes)"
-L[29]="DataMemory (KBytes)"
-L[30]="LibMemory (KBytes)"
-L[31]="ConnHdelta"
-L[32]="ConnCdelta"
-L[33]="XactionsDelta"
-L[34]="Client Pool Delta"
-L[35]="Spare Threads Delta"
-L[36]="Active Threads Delta"
-L[37]="Threads Waiting Delta"
-L[38]="Threads Starting up Delta"
-L[39]="Threads Prefetching Delta"
-L[40]="Threading Errors Delta"
-L[41]="Outbound Connections created Delta"
-L[42]="Outbound Connections Failed Delta"
-L[43]="Outbound Connection Pool Reused Delta"
-L[44]="Outbound Connections in Pool Delta"
-L[45]="KBytesInDelta"
-L[46]="KBytesOutDelta"
-L[47]="Caching Objects Created in Memory Delta"
-L[48]="Caching Objects Removed from Memory Delta"
-L[49]="DNS Queries Reused Delta"
-L[50]="New DNS Queries Delta"
-L[51]="DNS Query failures Delta"
-L[52]="LoadAvg1"
-L[53]="LoadAvg5"
-L[54]="LoadAvg15"
-L[55]="RProcesses"
-L[56]="WProcesses"
-L[57]="U.Time"
-L[58]="SysTime"
-L[59]="U+SysTime"
-L[60]="UTimeD"
-L[61]="SysTimeD"
-L[62]="U+SysTimeD"
+L[0]="TStamp"
+L[1]="ElapsedT"
+L[2]="inAccept"
+L[3]="inClosed"
+L[4]="xactions"
+L[5]="inIdle"
+L[6]="thSpare"
+L[7]="thActive"
+L[8]="thWait"
+L[9]="thSpcl"
+L[10]="thPftch"
+L[11]="thError"
+L[12]="outCreate"
+L[13]="outFail"
+L[14]="outReuse"
+L[15]="outIdle"
+L[16]="BytesIn"
+L[17]="BytesOut"
+L[18]="cchNew"
+L[19]="cchRem"
+L[20]="dnsHit"
+L[21]="dnsMiss"
+L[22]="dnsFail"
+L[23]="memSysT"
+L[24]="memSysF"
+L[25]="memV"
+L[26]="memRes"
+L[27]="memRSS"
+L[28]="memCode"
+L[29]="memData"
+L[30]="memLib"
+L[31]="inAcceptD"
+L[32]="inClosedD"
+L[33]="xactionD"
+L[34]="inIdleD"
+L[35]="thSpareD"
+L[36]="thActiveD"
+L[37]="thWaitD"
+L[38]="thSpclD"
+L[39]="thPftchD"
+L[40]="thErrorD"
+L[41]="outCreatD"
+L[42]="outFailD"
+L[43]="outReuseD"
+L[44]="outIdleD"
+L[45]="BytesInD"
+L[46]="BytesOutD"
+L[47]="cchNewD"
+L[48]="cchRemD"
+L[49]="dnsHitD"
+L[50]="dnsMissD"
+L[51]="dnsFailD"
+L[52]="loadAv1"
+L[53]="loadAv5"
+L[54]="loadAv15"
+L[55]="prcRun"
+L[56]="prcWait"
+L[57]="tmUserT"
+L[58]="tmSysT"
+L[59]="tmTotal"
+L[60]="tmUserD"
+L[61]="tmSysD"
+L[62]="tmTotalD"
+
+
+HEAD()
+{
+printf '%16.16s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %12.8s %12.8s %12.8s %12.8s %12.8s %12.8s\n' \
+			"${L[0]}" "${L[1]}" "${L[2]}" "${L[3]}" "inConcrt" "${L[4]}" "${L[33]}" "${L[57]}" "${L[58]}" "${L[59]}" "${L[62]}" "sysYield"
+}
 
 
 OIFS=${IFS}
 IFS=','
-tail -n 500 -F /var/log/safesquid/performance/performance.log | while read -a P
+S=$(wc -l < /var/log/safesquid/performance/performance.log)
+tail -n $[ S - 5 ] -F /var/log/safesquid/performance/performance.log | while read -a P
 do	
-	M=`tput lines`
+	M=$(tput lines)
 	M=$(( M - 2 ))
 	i=$(( i + 1 ))
-	[ $i == 1 ] && printf '%16.16s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s %8.8s\n' ${L[0]} ${L[1]} ${L[2]} ${L[3]} ${L[4]} ${L[31]} ${L[32]} ${L[32]} ${L[5]} ${L[15]} ${L[25]} ${L[26]} ${L[60]} ${L[61]}
-				   printf '%16d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d % 8.4f % 8.4f\n'                    ${P[0]} ${P[1]} ${P[2]} ${P[3]} ${P[4]} ${P[31]} ${P[32]} ${P[33]} ${P[5]} ${P[15]} ${P[25]} ${P[26]} ${P[60]} ${P[61]}
-	[ $i == $M ] && let i=0
+	
+	[ $i == 1 ] && HEAD;
+    let z=$(echo "${P[33]} != 0" | bc -l)
+    let y=$(echo "${P[59]} > 0" | bc)
+    [ 1 -eq "$z" ] && [ 1 -eq "$y" ] && t=$(echo "scale=4; ${P[4]} / ${P[59]}" | bc -l)
+
+	printf '%16d %8d %8d %8d %8d %8d %8d %12.4f %12.4f %12.4f %12.4f %12.4f\n' \
+		"${P[0]}" "${P[1]}" "${P[2]}" "${P[3]}" "$(( P[2] - P[3] ))" "${P[4]}" "${P[33]}" "${P[57]}" "${P[58]}" "${P[59]}" "${P[62]}" "$t"
+	
+	[ $i == $M ] && i=0
 done
 
 IFS=${OIFS}
+
+exit;
+
